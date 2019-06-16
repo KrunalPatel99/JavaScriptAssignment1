a5203cd24ff05c3cec3ae297a3e156320f8ec2ce
KRUNAL PATEL <kp14127@gmail.com>
2019-06-16 16:42:42 -0400

    Addded comments and resolved navigation link issue

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
+//added route to about page
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About' });
 });
-
+//added route to services page
 router.get('/services', function(req, res, next) {
   res.render('services', { title: 'Services' });
 });
 
-
+//added route to contact page
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
 });
-
+//added route to projects page
 router.get('/projects', function(req, res, next) {
   res.render('projects', { title: 'Projects' });
 });
