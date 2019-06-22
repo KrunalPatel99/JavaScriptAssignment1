9e3d3a18c5d545d8634ab83386e372e4aaebc3a8
root <root@vmi224042.contaboserver.net>
2019-06-22 17:38:29 -0400

    Working Contact Form

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost:27017/submitrequest');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+    
+
+});
+
+
+var SCContact = mongoose.Schema({
+      fullName     :    String,
+      email        :    String,
+      tel          :    String,
+      website      :    String,
+      msg          :    String
+    
+    });
+
+var Contact = mongoose.model('Contact', SCContact, 'submitrequest');
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
@@ -24,4 +52,22 @@ router.get('/projects', function(req, res, next) {
   res.render('projects', { title: 'Projects' });
 });
 
+router.post('/submitrequest', function (req, res) {
+
+    console.log("");
+    //res.send(req.body);
+res.send("Contact Request Submitted")
+
+    
+//var newcontact = new data(req.body);
+var contact1 = new Contact(req.body);
+
+contact1.save(function (err, contact) {
+      if (err) return console.error(err);
+    
+    console.log("Data Saved Successfully");
+
+});
+
+});
 module.exports = router;
